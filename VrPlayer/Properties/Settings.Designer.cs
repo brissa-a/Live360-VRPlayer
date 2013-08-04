59979dbcd2567d34ba5929099d9a06322a519ab7
stephanelx <stephanelx@gmail.com>
2013-08-04 00:11:29 -0400

    Updated PeterM example and default config+settings

--- VrPlayer/Properties/Settings.Designer.cs
+++ VrPlayer/Properties/Settings.Designer.cs
@@ -169,7 +169,7 @@ namespace VrPlayer.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        [global::System.Configuration.DefaultSettingValueAttribute("SideBySide")]
         public string LayoutMode {
             get {
                 return ((string)(this["LayoutMode"]));
