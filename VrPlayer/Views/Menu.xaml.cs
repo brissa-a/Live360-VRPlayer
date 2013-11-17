e1d1424892fce53beaefec602b59ae830c69fde3
stephanelx <stephanelx@gmail.com>
2013-11-16 21:52:45 -0500

    Updated UI with presets menu

--- VrPlayer/Views/Menu.xaml.cs
+++ VrPlayer/Views/Menu.xaml.cs
@@ -1,22 +1,38 @@
 ﻿using System;
 using System.Windows;
-using System.Windows.Controls;
+using System.Windows.Forms;
 using VrPlayer.Helpers;
+using VrPlayer.ViewModels;
+using Application = System.Windows.Application;
+using UserControl = System.Windows.Controls.UserControl;
 
 namespace VrPlayer.Views
 {
     public partial class Menu : UserControl
     {
+        private readonly MenuViewModel _viewModel;
+
         public Menu()
         {
             InitializeComponent();
             try
             {
-                DataContext = ((App)Application.Current).ViewModelFactory.CreateMenuViewModel();
+                _viewModel = ((App)Application.Current).ViewModelFactory.CreateMenuViewModel();
+                DataContext = _viewModel;
             }
             catch (Exception exc)
             {
                 Logger.Instance.Error("Error while initilizing Menu view.", exc);
+            }
+        }
+
+        private void SavePresetMenuItem_OnClick(object sender, RoutedEventArgs e)
+        {
+            var saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = @"VR Player Presets|*.json";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                _viewModel.SaveMediaPresetCommand.Execute(saveFileDialog.FileName);
             }
         }
     }
